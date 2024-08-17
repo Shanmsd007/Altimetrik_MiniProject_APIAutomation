60c6b91599225ba2d9e8de12233969211d9a417c
Shanmugam <shanpsg146@gmail.com>
2024-08-17 19:25:40 +0530

    Eclipse IDE - Updated getUserInfo test case

--- src/test/java/apiTestCases/GetUserInfoTest.java
+++ src/test/java/apiTestCases/GetUserInfoTest.java
@@ -4,17 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import helpers.GetUserInfoAPI;
+import io.restassured.response.Response;
 import pojo.UserDetails;
 import utilities.DataProviders;
 
 public class GetUserInfoTest {
 
 	@Test(dataProvider = "ProgData", dataProviderClass = DataProviders.class)
-	public void validateUserInfoTest(String Name, String Age) {
-
-		UserDetails userDetails = GetUserInfoAPI.ValidateUserInfo(Name);
-		Assert.assertEquals(userDetails.getName(), Name);
-		Assert.assertEquals(userDetails.getAge(), Age);
+	public void getUserInfo(String Name, String Age) {
+		Response response = GetUserInfoAPI.getUserInfo(Name);
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+		System.out.println("The response code is " + statusCode);
 
 	}
 }
