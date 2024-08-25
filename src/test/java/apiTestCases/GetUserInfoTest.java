205daccf36c4c4181115549e13d2cbdc8787b85e
Shanmugam <shanpsg146@gmail.com>
2024-08-25 21:18:28 +0530

    IntelliJ IDE - Updated softAssert and try/catch/finally block in the scripts

--- src/test/java/apiTestCases/GetUserInfoTest.java
+++ src/test/java/apiTestCases/GetUserInfoTest.java
@@ -5,17 +5,27 @@ import org.testng.annotations.Test;
 
 import helpers.GetUserInfoAPI;
 import io.restassured.response.Response;
+import org.testng.asserts.SoftAssert;
 import pojo.UserDetails;
 import utilities.DataProviders;
 
 public class GetUserInfoTest {
 
+	SoftAssert softAssert = new SoftAssert();
+
 	@Test(dataProvider = "ProgData", dataProviderClass = DataProviders.class)
 	public void getUserInfo(String Name, String Age) {
-		Response response = GetUserInfoAPI.getUserInfo(Name);
-		int statusCode = response.getStatusCode();
-		Assert.assertEquals(statusCode, 200);
-		System.out.println("The response code is " + statusCode);
+		try {
+			Response response = GetUserInfoAPI.getUserInfo(Name);
+			int statusCode = response.getStatusCode();
+			Assert.assertEquals(statusCode, 200);
+			System.out.println("The response code is " + statusCode);
+		} catch (Exception e) {
+			System.out.println("Error in getting the user information" +e.getMessage() +e.getCause());
+			throw new RuntimeException(e);
+		} finally {
+			softAssert.assertAll();
+		}
 
 	}
 }
--- src/test/java/apiTestCases/ValidateUserInfoTest.java
+++ src/test/java/apiTestCases/ValidateUserInfoTest.java
@@ -5,17 +5,26 @@ import org.testng.annotations.Test;
 
 import helpers.GetUserInfoAPI;
 import io.restassured.response.Response;
+import org.testng.asserts.SoftAssert;
 import pojo.UserDetails;
 import utilities.DataProviders;
 
 public class ValidateUserInfoTest {
+	SoftAssert softAssert = new SoftAssert();
 
 	@Test(dataProvider = "ProgData", dataProviderClass = DataProviders.class)
 	public void validateUserInfoTest(String Name, String Age) {
 
-		UserDetails userDetails = GetUserInfoAPI.ValidateUserInfo(Name);
-		Assert.assertEquals(userDetails.getName(), Name);
-		Assert.assertEquals(userDetails.getAge(), Age);
+		try {
+			UserDetails userDetails = GetUserInfoAPI.ValidateUserInfo(Name);
+			softAssert.assertEquals(userDetails.getName(), Name);
+			softAssert.assertEquals(userDetails.getAge(), Age);
+		} catch (Exception e) {
+			System.out.println("Error in validating the user information" +e.getMessage() +e.getCause());
+			throw new RuntimeException(e);
+		} finally {
+			softAssert.assertAll();
+		}
 
 	}
 }
